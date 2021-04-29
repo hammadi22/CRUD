60bea65c64fc04c22ee1f756013a37069cfaddac
hammadi22 <hammdi1996.houssem@gmail.com>
2021-04-29 10:30:18 +0100

    add port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ connectDB();
 
 app.use("/persons", personRoute);
 
-const port = process.env.PORT;
+const port = 5000;
 app.listen(port, (err) =>
   err ? console.log("error") : console.log(`server is running on port ${port}`)
 );
